55849bca97c2786bac8079e2fd4c623b26bac6f4
entr0pee <71291676+entr0pee@users.noreply.github.com>
2020-09-28 19:39:26 -0400

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,17 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
+
+# Add luci-app-jd-dailybonus
+git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-jd-dailybonus.git
+
+# Add luci-app-vssr
+git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-vssr.git
